02de9613d37d7006059516664320177279991c9f
Dhia Mbarki <tahridhia22@gmail.com>
2021-04-07 00:54:05 +0100

    hashing password

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -1,46 +1,40 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose')
-const schema = mongoose.model("schema")
+const mongoose = require("mongoose");
+const schema = mongoose.model("schema");
+const bcrypt = require("bcryptjs");
 
+router.post("/signup", (req, res) => {
+  const { name, email, password } = req.body;
+  if (!email || !password || !name) {
+    return res.status(422).json({ error: "try to add your informations" });
+  }
+  schema
+    .findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res.status(422).json({ error: "user is already exist!" });
+      }
+      bcrypt.hash(password, 15).then((hahedpassword) => {
+        const user = new schema({
+          email,
+          password: hahedpassword,
+          name
+        });
 
-router.get("/", (req, res) => {
-  res.send("hey");
+        
+        user.save()
+          .then((user) => {
+            res.json({ message: " data saved" });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-
-
-router.post('/signup', (req,res)=> {
-const{name,email,password} = req.body
-if(!email || !password || !name) {
-   return res.status(422).json({error:"try to add your informations"})
-}
-schema.findOne({email:email}).then((savedUser)=>{
-  
-  if(savedUser){
-    return res.status(422).json({error:"user is already exist!"})
-
-  }
-  const user = new schema({
-    email,
-    password,
-    name
-  })
-  user.save()
-  .then(user=>{
-    res.json({message:" data saved"})
-  })
-  .catch(err=>{
-    console.log(err)
-  })
- 
-})
-.catch(err=>{
-  console.log(err)
-})
-
-})
-
-
-
-module.exports = router
+module.exports = router;
