b758c6c1ef2db2fa43d3be3b09d35fa7a10e4ced
Dhia Mbarki <tahridhia22@gmail.com>
2021-04-06 20:46:16 +0100

    add mongodb

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const app = express()
 
-const PORT = 8000
+const PORT = 8000;
+// Js2bFlOsoetlNQNk
 
+const customMiddleware = (req,res,next) =>{
+    console.log("middleware working fine" )
+    next()
+}
+app.use(customMiddleware)
 
 
 app.get('/', (req,res)=>{
+    console.log("test")
     res.send("hey there")
 })
+
+
+app.get('/about',customMiddleware, (req,res)=>{
+    console.log("about")
+    res.send("about page")
+})
+
+
+
 
 app.listen(PORT,()=> {
     console.log("server jawou behi", PORT)
---
+++ server/mongo.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI:"mongodb+srv://dhia:Js2bFlOsoetlNQNk@cluster0.6ikp4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
