ff0e3b6fdc7720e03014c5ba7d72b4c84ab25141
Dhia Mbarki <tahridhia22@gmail.com>
2021-04-07 13:46:09 +0100

    signin route Done

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -42,7 +42,26 @@ router.post("/signup", (req, res) => {
 router.post('/signin',(req,res)=>{
   const {email,password} = req.body
   if(!email || !password){
-    res.status(422).json()
+    res.status(422).json({error:"add your email or password"})
   }
+  schema.findOne({email:email})
+  .then(savedUser=> {
+    if(!savedUser) {
+    return res.status(422).json({error:"Invalid Email or password"})
+    }
+    bcrypt.compare(password,savedUser.password )
+    .then(doMatch=>{
+      if(doMatch) {
+        res.json({message:"successfully signed in"})
+      }
+      else{
+        return res.status(422).json({error:"Invalid Email or password"})
+
+      }
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  })
 })
 module.exports = router;
