259c49bf481ba298db9f8c9ce1383ab4f62aa03d
Dhia Mbarki <tahridhia22@gmail.com>
2021-04-08 13:41:13 +0100

    add navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Nav from './components/Nav';
-
+import "./App.css"
+import {BrowserRouter,Route} from 'react-router-dom'
+import Home from './components/home/Home'
+import Profile from './components/home/Profile'
+import Signin from './components/home/Signin'
+import Signup from './components/home/Signup'
 
 function App() {
   return (
+    <BrowserRouter>
 <Nav/>  
+
+
+
+</BrowserRouter>
 );
 }
 
--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -3,15 +3,15 @@ import React from 'react'
 function Nav() {
     return (
         <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right ">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
+        <div class="nav-wrapper white">
+          <a href="#" class="brand-logo right">Logo</a>
+          <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li><a href="sass.html">signin</a></li>
+            <li><a href="badges.html">signup</a></li>
+            <li><a href="collapsible.html">Profile</a></li>
+          </ul>
+        </div>
+      </nav>
     )
 }
 
