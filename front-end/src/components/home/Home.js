de9860cf0c2935d3e08e3f46a5277208da48a140
Dhia Mbarki <tahridhia22@gmail.com>
2021-04-08 20:03:10 +0100

    working on home page

--- front-end/src/components/home/Home.js
+++ front-end/src/components/home/Home.js
@@ -4,7 +4,17 @@ import React from 'react'
 function Home() {
     return (
         <div>
-            <h1>home</h1>
+            <div card home-card>
+                <h5>Dhia</h5>
+                <div className="card-image">
+                    <img src ="https://images.unsplash.com/photo-1595975284442-e279aeb94bd7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"/>
+                </div>
+                <div className="card-content">
+                    <h6>title</h6>
+                    <p>don't give up :)</p>
+                    <input type="text" placeholder="comment.."></input>
+                </div>
+            </div>
         </div>
     )
 }
--- front-end/src/components/home/Profile.js
+++ front-end/src/components/home/Profile.js
@@ -3,7 +3,7 @@ import "./profile.css"
 
 function Profile() {
   return (
-    <div>
+    <div style={{maxWidth:"1300px",margin:"0px auto"}}>
       <div
         style={{
           display: "flex",
@@ -18,7 +18,7 @@ function Profile() {
             src="https://scontent.ftun4-1.fna.fbcdn.net/v/t1.6435-9/157381447_2860189534263179_1326743122113976050_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=LFLUSqMx_vAAX8ksLnl&_nc_ht=scontent.ftun4-1.fna&oh=4bbdc176b308274bfd4a80859da0fa13&oe=6094D0C8"
           />
         </div>
-        <div>
+        <div className="mov">
           <h4>Dhia-Mbarki</h4>
           <div
             style={{
