80f28961e3055e92e4d0bd75cd2853bbcb2f4eec
Dhia Mbarki <tahridhia22@gmail.com>
2021-04-11 20:35:48 +0100

    fix routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect,createContext,useReducer,useContext} from 'react';
 import Nav from "./components/Nav";
 import Home from "./components/home/Home";
 import "./App.css";
@@ -10,6 +10,31 @@ import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import AddPost from "./components/home/AddPost";
 import Profile from "./components/home/profile/Profile";
+
+
+const Routing =()=> {
+  return(
+    <switch>
+    <Route exact path="/">
+    <Home />
+  </Route>
+  <Route path="/signin">
+    <Signin />
+  </Route>
+  <Route path="/signup">
+    <Signup />
+  </Route>
+
+  <Route path="/profile">
+    <Profile />
+  </Route>
+  <Route path="/addPost">
+    <AddPost />
+  </Route>
+  </switch>
+  )
+}
+
 function App() {
   
   return (
@@ -17,22 +42,7 @@ function App() {
     <BrowserRouter>
       <Nav />
 
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/signin">
-        <Signin />
-      </Route>
-      <Route path="/signup">
-        <Signup />
-      </Route>
-
-      <Route path="/profile">
-        <Profile />
-      </Route>
-      <Route path="/addPost">
-        <AddPost />
-      </Route>
+     
     </BrowserRouter>
     </div>
   );
