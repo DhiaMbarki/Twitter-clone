2b5c0094fd4f2f731feaadccaaa3a8c4c1da4630
Dhia Mbarki <tahridhia22@gmail.com>
2021-04-16 22:30:01 +0100

    delete card

---
+++ client/src/components/Post/DeleteCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deletePost } from "../../actions/post.actions";
+
+const DeleteCard = (props) => {
+  const dispatch = useDispatch();
+
+  const deleteQuote = () => dispatch(deletePost(props.id));
+
+  return (
+    <div
+      onClick={() => {
+        deleteQuote();
+      }}
+    >
+      <img src="./img/icons/trash.svg" alt="trash" />
+    </div>
+  );
+};
+
+export default DeleteCard;
