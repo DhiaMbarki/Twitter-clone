972d84085ed8798a8cf689c96ee1d1666baa0878
Dhia Mbarki <tahridhia22@gmail.com>
2021-04-16 22:28:34 +0100

    card form

---
+++ client/src/components/Post/Card.js
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { dateParser, isEmpty } from "../Utils";
+import FollowHandler from "../Profil/FollowHandler";
+import LikeButton from "./LikeButton";
+import { updatePost } from "../../actions/post.actions";
+import DeleteCard from "./DeleteCard";
+import CardComments from "./CardComments";
+
+const Card = ({ post }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [textUpdate, setTextUpdate] = useState(null);
+  const [showComments, setShowComments] = useState(false);
+  const usersData = useSelector((state) => state.usersReducer);
+  const userData = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+
+  const updateItem = () => {
+    if (textUpdate) {
+      dispatch(updatePost(post._id, textUpdate));
+    }
+    setIsUpdated(false);
+  };
+
+  useEffect(() => {
+    !isEmpty(usersData[0]) && setIsLoading(false);
+  }, [usersData]);
+
+  return (
+    <li className="card-container" key={post._id}>
+      {isLoading ? (
+        <i className="fas fa-spinner fa-spin"></i>
+      ) : (
+        <>
+          <div className="card-left">
+            <img
+              src={
+                !isEmpty(usersData[0]) &&
+                usersData
+                  .map((user) => {
+                    if (user._id === post.posterId) return user.picture;
+                    else return null;
+                  })
+                  .join("")
+              }
+              alt="poster-pic"
+            />
+          </div>
+          <div className="card-right">
+            <div className="card-header">
+              <div className="pseudo">
+                <h3>
+                  {!isEmpty(usersData[0]) &&
+                    usersData
+                      .map((user) => {
+                        if (user._id === post.posterId) return user.pseudo;
+                        else return null;
+                      })
+                      .join("")}
+                </h3>
+                {post.posterId !== userData._id && (
+                  <FollowHandler idToFollow={post.posterId} type={"card"} />
+                )}
+              </div>
+            </div>
+            {isUpdated === false && <p>{post.message}</p>}
+            {isUpdated && (
+              <div className="update-post">
+                <textarea
+                  defaultValue={post.message}
+                  onChange={(e) => setTextUpdate(e.target.value)}
+                />
+                <div className="button-container">
+                  <button className="btn" onClick={updateItem}>
+                    change{" "}
+                  </button>
+                </div>
+              </div>
+            )}
+            {post.picture && (
+              <img src={post.picture} alt="card-pic" className="card-pic" />
+            )}
+            {post.video && (
+              <iframe
+                width="500"
+                height="300"
+                src={post.video}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title={post._id}
+              ></iframe>
+            )}
+            {userData._id === post.posterId && (
+              <div className="button-container">
+                <div onClick={() => setIsUpdated(!isUpdated)}>
+                  <h5>edit</h5>{" "}
+                </div>
+                <DeleteCard id={post._id} />
+              </div>
+            )}
+            <div className="card-footer">
+              <div className="comment-icon">
+                <button
+                  className="mov"
+                  onClick={() => setShowComments(!showComments)}
+                >
+                  comment
+                </button>
+              </div>
+              <LikeButton post={post} />
+            </div>
+            {showComments && <CardComments post={post} />}
+          </div>
+        </>
+      )}
+    </li>
+  );
+};
+
+export default Card;
