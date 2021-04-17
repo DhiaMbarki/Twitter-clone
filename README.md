acace290af1d002c6708ef0dee908fca0df3e684
Dhia Mbarki <tahridhia22@gmail.com>
2021-04-17 23:21:22 +0100

    refac

--- README.md
+++ README.md
@@ -2,20 +2,7 @@
  
  
  
- ![2](https://user-images.githubusercontent.com/66827110/115127350-be045880-9fcd-11eb-9601-ed3454a4beb8.png)
-
-
-
-
-![3](https://user-images.githubusercontent.com/66827110/115127364-cceb0b00-9fcd-11eb-8c17-6b07056e0ee3.png)
-
-
-
-
-
-![15](https://user-images.githubusercontent.com/66827110/115127390-f6a43200-9fcd-11eb-8a93-8bd5472ae44e.png)
-
-
+ 
 
 
 
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 const postRoutes = require('./routes/post.routes');
-require('dotenv').config({path: './db/.env'});
-require('./db/db');
+require('dotenv').config({path: './config/.env'});
+require('./config/db');
 const {checkUser, requireAuth} = require('./middleware/auth.middleware');
 const cors = require('cors');
 
